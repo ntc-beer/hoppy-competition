bbff298e01df344e91aebca4b6e37fbabf53738b
Barry Forrest <barry.forrest@gmail.com>
2026-02-03 13:57:39 -0600

    on my way :-)

---
+++ src/content.config.ts
@@ -0,0 +1,31 @@
+import { defineCollection } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+const clubs = defineCollection({
+  loader: file("../config/clubs.json"),
+  schema: z.object({
+    name: z.string(),
+    website: z.string().url().optional()
+  })
+});
+
+const competitions = defineCollection({
+  loader: file("../config/competitions.json"),
+  schema: z.object({
+    name: z.string(),
+    openDate: z.string(),
+    closeDate: z.string(),
+    awardsDate: z.string().optional(),
+    location: z.string().optional(),
+    club: z.string().optional(),
+    infoLink: z.string().url().optional()
+  })
+});
+
+export const collections = {clubs, competitions};
+
